6ccdc4320044b6b585e1143ba8136c89b0a41ed9
Anthony Chou <achou16@gmail.com>
2020-09-10 13:45:29 -0700

    add buttons and search field place holders, add to app.test

--- src/containers/PokemonList.jsx
+++ src/containers/PokemonList.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 export default function PokemonListContainer() {
+    const noop = () => null;
     return (
         <div className="pokemon-list-container">
-            list container
+            <div className="list-button-container">
+                <div>
+                    <button className="list-button" onClick={noop}>All</button>
+                    <button className="list-button" onClick={noop}>Saved</button>
+                </div>
+            </div>
+            <div className="search-container">
+                <input type="text" placeholder="Search" onChange={noop} />
+            </div>
         </div>
     );
 }
