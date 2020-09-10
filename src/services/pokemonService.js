23d719109ed2b204ba5e9f5351abd8d219aff234
Anthony Chou <achou16@gmail.com>
2020-09-10 13:40:16 -0700

    add getPokemons service

---
+++ src/services/pokemonService.js
@@ -0,0 +1,7 @@
+const pokeApiURL = 'https://pokeapi.co/api/v2/pokemon';
+const locationApiURL = 'https://api.craft-demo.net/pokemon/';
+const apiKey = 'HHko9Fuxf293b3w56zAJ89s3IcO9D5enaEPIg86l';
+
+export async function getPokemons() {
+    return fetch(`${pokeApiURL}`);
+}
