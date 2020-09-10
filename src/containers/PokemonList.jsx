5b2092ea0831fa1ffe603278f75824b3f93668b0
Anthony Chou <achou16@gmail.com>
2020-09-10 13:53:39 -0700

    fetch data and add methods for flipping between all/saved

--- src/containers/PokemonList.jsx
+++ src/containers/PokemonList.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
+import { getPokemons } from '../services/pokemonService';
 
 export default function PokemonListContainer() {
-    const noop = () => null;
+    const [pokemons, setPokemons] = useState([]);
+
+    const fetchData = () => {
+        getPokemons()
+        .then(res => res.json())
+        .then(data => {
+            setPokemons(data.results);
+        });
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const search = (e) => {
+
+    }
+
+    const toggleAll = () => {
+        setPokemons(pokemons);
+        console.log('all')
+    }
+    const toggleSaved = () => {
+        setPokemons(pokemons);
+        console.log('saved')
+    }
+
     return (
         <div className="pokemon-list-container">
             <div className="list-button-container">
                 <div>
-                    <button className="list-button" onClick={noop}>All</button>
-                    <button className="list-button" onClick={noop}>Saved</button>
+                    <button className="list-button" onClick={toggleAll}>All</button>
+                    <button className="list-button" onClick={toggleSaved}>Saved</button>
                 </div>
             </div>
             <div className="search-container">
-                <input type="text" placeholder="Search" onChange={noop} />
+                <input type="text" placeholder="Search" onChange={search} />
             </div>
+            {/* <PokemonList pokemons={pokemons} /> */}
         </div>
     );
 }
