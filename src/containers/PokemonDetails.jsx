502d9ac5ebd0d18bd0f45123a48fab2fb669f4a8
Anthony Chou <achou16@gmail.com>
2020-09-15 12:06:27 -0700

    add an action live!

--- src/containers/PokemonDetails.jsx
+++ src/containers/PokemonDetails.jsx
@@ -1,28 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import PokemonDetails from '../components/PokemonDetails';
 import { getPokemonLocation } from '../services/pokemonService';
-import { usePokemonState, useAppDispatch, useSavedState } from '../context/AppContext';
+import { usePokemonState, useAppDispatch, useSavedState, usePokemonLocation } from '../context/AppContext';
 
 export default function PokemonDetailsContainer(props) {
-    const [locations, setPokemonLocations] = useState({});
     const pokemon = usePokemonState();
     const dispatch = useAppDispatch();
     const savedState = useSavedState();
     const saved = savedState.find((p) => p.id === pokemon.id);
+    const [locations, setPokemonLocations] = useState(usePokemonLocation(pokemon.name));
     console.log(saved);
     const fetchData = async (id) => {
         await getPokemonLocation(id)
             .then(res => res.json())
             .then(data => {
                 setPokemonLocations(data.locations)
+                dispatch({type: 'SAVE_POKEMON_LOCATION',
+                    payload: {name: pokemon.name, locations:data.locations}});
             });
     }
 
     useEffect(() => {
-        fetchData(pokemon.id);
+        if (locations === null) {
+            fetchData(pokemon.id);
+        }
     }, []);
 
-    // useEffect(() => {console.log('pokemon locations: ',locations)}, [locations]);
+    useEffect(() => {console.log('pokemon locations: ',locations)}, [locations]);
 
     return (
         <div className="pokemon-details-container">
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -8,45 +8,59 @@ const initState = {
     currentPokemon: {},
     savedPokemon: [],
     // currentURL: "",
-    pokemons: {}
+    pokemons: {},
+    pokemonLocations: {}
 }
 
 const appReducer = (state, action) => {
     switch (action.type) {
+        case "SAVE_POKEMON_LOCATION":
+            return {
+                pokemonList: state.pokemonList,
+                currentPokemon: state.currentPokemon,
+                savedPokemon: state.savedPokemon,
+                pokemons: state.pokemons,
+                pokemonLocations: storePokemonLocation(state.pokemonLocations, action.payload)
+            }
         case "SAVED_POKEMON_ADD":
             return {
                 pokemonList: state.pokemonList,
                 currentPokemon: state.currentPokemon,
                 savedPokemon: [...state.savedPokemon, action.payload],
-                pokemons: state.pokemons
+                pokemons: state.pokemons,
+                pokemonLocations: state.pokemonLocations
             };
         case "SAVED_POKEMON_DELETE":
             return {
                 pokemonList: state.pokemonList,
                 currentPokemon: state.currentPokemon,
                 savedPokemon: state.savedPokemon.filter(pokemon => pokemon.id !== action.payload.id),
-                pokemons: state.pokemons
+                pokemons: state.pokemons,
+                pokemonLocations: state.pokemonLocations
             };
         case "SET_CURRENT_POKEMON":
             return {
                 pokemonList: state.pokemonList,
                 currentPokemon: action.payload,
                 savedPokemon: state.savedPokemon,
-                pokemons: state.pokemons
+                pokemons: state.pokemons,
+                pokemonLocations: state.pokemonLocations
             }
         case "STORE_POKEMONS":
             return {
                 pokemonList: action.payload,
                 currentPokemon: state.currentPokemon,
                 savedPokemon: state.savedPokemon,
-                pokemons: state.pokemons
+                pokemons: state.pokemons,
+                pokemonLocations: state.pokemonLocations
             }
         case "STORE_POKEMON":
             return {
                 pokemonList: state.pokemonList,
                 currentPokemon: state.currentPokemon,
                 savedPokemon: state.savedPokemon,
-                pokemons: storePokemon(state.pokemons, action.payload)
+                pokemons: storePokemon(state.pokemons, action.payload),
+                pokemonLocations: state.pokemonLocations
             }
         default:
             return state;
@@ -57,6 +71,21 @@ const storePokemon = (state, payload) => {
     let newState = Object.assign({}, state);
     newState[payload.name] = payload;
     return newState;
+}
+
+const storePokemonLocation = (state, payload) => {
+    let newState = Object.assign({}, state);
+    newState[payload.name] = payload.locations;
+    return newState;
+}
+
+export const usePokemonLocation = (name) => {
+    const context = useAppState();
+    if (name in context.pokemonLocations) {
+        return context.pokemonLocations[name];
+    } else {
+        return null;
+    }
 }
 
 
